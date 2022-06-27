8877af61f97ede1bd2e96bd03817139039ba09e0
Yey223 <104279031+Yey223@users.noreply.github.com>
2022-06-27 18:13:55 +0700

    Update _rules.js

--- plugins/_rules.js
+++ plugins/_rules.js
@@ -24,20 +24,20 @@ let krizyn = `
            hydratedButtons: [{
              urlButton: {
                displayText: '🤖GroupBot🤖',
-               url: 'https://chat.whatsapp.com/Hs5rHr27xu49VRLgdliMfR'
+               url: 'https://chat.whatsapp.com'
              }
 
            },
                 {
                urlButton: {
                displayText: 'YOUTUBE ME',
-               url: 'https://youtu.be/tH-0Uq_s_H4'
+               url: 'https://youtu.be/tegarrzzzzyhh'
              }
 
            },
                {
              quickReplyButton: {
-               displayText: 'JANGAN DIPENCET',
+               displayText: 'tegar ganteng 😎',
                id: '',
              }
              
